166d41812f8fc97a338ee4f797f5009204462c52
Kevin Smith <kevin@kevinsmith.io>
2025-01-31 15:31:00 -0600

    Make a test edit

--- app/src/HttpServer/TempWelcome.php
+++ app/src/HttpServer/TempWelcome.php
@@ -15,9 +15,11 @@ final readonly class TempWelcome
     <title>CFB Marble Game</title>
 </head>
 <body>
+
 <h1>CFB Marble Game</h1>
 <p>Simple Rules for a Complex Season</p>
 <p><a href="https://x.com/CFBMarbleGame">More</a></p>
+
 </body>
 </html>
 HTML);
